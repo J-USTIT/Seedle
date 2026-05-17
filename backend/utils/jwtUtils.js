968cfcb6d2c73ea58e82cb3081fa9bb0f457b9ab
jbernards728 <joaquimnathaniel.bernardo.cics@ust.edu.ph>
2026-05-17 14:55:30 +0800

    Added a middleware for verifying tokens

---
+++ backend/utils/jwtUtils.js
@@ -0,0 +1,36 @@
+import jwt from "jsonwebtoken";
+
+/**
+ * JWT (Json Web Token) for the user
+ * @param {string} userId user Mongodb ID
+ * @param {string} email user email
+ * @returns {string} JWT token
+ */
+
+export const generateToken = (userid, email) => {
+        const jwtToken = jwt.sign(
+            { userId, email}, 
+            process.env.JWT_SECRET || "this-is-nothehe-your-secret", // Your secret (use .env in production)
+            { expiresIn: "1d"} // Token expiry: 1 day
+        )
+        return jwtToken;
+};
+
+/**
+ * JWT token verification, returns decoded data
+ * @param {string} token - the token
+ * @returns {Object} - decoded token data (userId and email)
+ * @throws {Error} - checks if token is expired or invalid
+ */
+
+export const verifyToken = (token) => {
+    try {
+        const decoded = jwt.verify(
+            token,
+            process.env.JWT_SECRET || "this-is-nothehe-your-secret"
+        );
+        return decoded; // { userId, email, iat, exp }
+    } catch (err) {
+        throw new Error("Invalid or expired token: " + err.message);
+    }
+};
