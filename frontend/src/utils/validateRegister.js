174e07fa1158165615e144a20e148132c2dc7488
J-USTIT <justinjake.gasmen.cics@ust.edu.ph>
2026-09-06 17:33:03 +0800

    Initial commit

--- frontend/src/utils/validateRegister.js
+++
@@ -1,8 +0,0 @@
-export const validateRegister = ({ username, email, password, confirmPassword }) => {
-    const errors = {
-        username: {message: '', status: false},
-        email: {message: '', status: false},
-        password: {message: '', status: false},
-        confirmPassword: {message: '', status: false},
-    }
-}
